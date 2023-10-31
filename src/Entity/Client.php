cc520c424598a54878c457c1d9d09d0dbcd2fadf
donjmi <jldonne@gmail.com>
2023-10-31 18:29:48 +0100

    Update Code (UC) : param entities Api

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -6,9 +6,11 @@ use App\Repository\ClientRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
 
 /**
  * @ORM\Entity(repositoryClass=ClientRepository::class)
+ * @ApiResource()
  */
 class Client
 {
